ee11e873738a20c6601be9118bcb203a637e9d4b
Don Welch <dwelch@nait.ca>
2022-01-17 16:41:27 -0700

    class componenets Constructors, Dehaviours and Enums

--- src/CSharpGrammar/PracticeConsole/Employment.cs
+++ src/CSharpGrammar/PracticeConsole/Employment.cs
@@ -87,7 +87,10 @@ namespace PracticeConsole.Data
         //the system manages the internal storage for the accessor and mutator
         //this is NO additional logic applied to the data value
 
-        public int Level { get; set; }
+        //using an enum for this field will AUTOMATICALLY restrict the values
+        //  this property can contain
+        public SupervisoryLevel Level { get; set; }
+
 
         //this property Years could be coded as either a fully implemented property
         //  or as an auto-implmented property
@@ -99,9 +102,73 @@ namespace PracticeConsole.Data
 
 
         //constructors
+        //is to initialize the physical object (instance) during its creation
+        //the result of creation is ensure that the coder gets an instance in
+        //   a known state
+        //
+        //if your class definition has NO constructor coded, then the data members /
+        //  auto implemented properties are set to the C# default data type value
+        //
+        //You can code one or more constructors in your class definition
+        //IF YOU CODE A CONSTRUCTOR FOR THE CLASS, YOU ARE RESPONSIBLE FOR ALL
+        //  CONSTRUCTORS USED BY THE CLASS!!!
+        //
+        //generally, if you are going to code your own constructor(s) you code two types
+        //  Default: this constructor does NOT take in any parameters (it mimics the default system constructor)
+        //  Greedy:  this constructor has list of parameters, on for each property, declare
+        //              for incoming data
+        //
+        // syntax: accesstype classname([list of parameters]) {constructor code body}
+        //
+        //IMPORTANT: The constructor DOES NOT have a return datatype
+        //           You DO NOT call a constructor directly, called using the new operator
+        //
+        //Default constructor
+        public Employment()
+        {
+            //constructor body
+            //a) empty
+            //b) you COULD assign literal values to your properties with this constructor
+            Level = SupervisoryLevel.TeamMember;
+            Title = "Unknown";
+        }
 
+        //Greedy constructor
+        public Employment(string title, SupervisoryLevel level, double years)
+        {
+            //constructor body
+            //a) a parameter for each property
+            //b) you COULD do validation within the constructor instead of the property
+            //c) validation for public readonly data members
+            //   validation for a property with a private set
 
+            Title = title;
+            Level = level;
+            Years = years;
+        }
 
-        //Behaviours
+        //Behaviours (aka methods)
+        //Behaviours are no different than methods elsewhere
+
+        //Syntax accesstype [static] returndatatype BehaviourName([list of parameters])
+        //          { code body }
+
+        //there maybe times you wish to obtain all the data in your instance
+        //  all at once for display
+        //generally to accomplish this, your class overrides the .ToString() method of classes
+
+        public override string ToString()
+        {
+            //comma separate value string (csv)
+            return $"{Title},{Level},{Years}";
+        }
+
+        public void SetEmployeeResponsibilityLevel(SupervisoryLevel level)
+        {
+            // you could do validation within this method to ensure acceptable value
+            if (level < 0)
+                throw new Exception("Responsibility Level must be positive");
+            Level = level;
+        }
     }
 }
