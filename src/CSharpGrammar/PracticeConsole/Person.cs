45e7e58dd51042ba2ad0c2895a08d65caf40aee1
Don Welch <dwelch@nait.ca>
2022-01-20 08:32:19 -0700

    Complete compoiste class Person

--- src/CSharpGrammar/PracticeConsole/Person.cs
+++ src/CSharpGrammar/PracticeConsole/Person.cs
@@ -9,6 +9,20 @@ namespace PracticeConsole.Data
     public class Person
     {
         //example of a composite class
+        //a composite class uses other classes in its definition
+        //a composite class is recongized with the phrase "has a" class
+        //this class of Person "has a" resident address
+
+        //an inherited class extends another class in its definition
+        //an inherited class is recongized with the phrase "is a" class
+        // assume a general class called Transportation
+        //then we can "extend" this class to more specific classes
+        //      public class Vehicle : Transportation
+        //      public class Bike : Transportation
+        //      public class Boat : Transportation
+
+
+
         //each instance of this class willrepresent an individual
         //This clas will define the following characteristics of a person
         //  First Name, Last Name, the current resident address, list of employment positions
@@ -20,7 +34,8 @@ namespace PracticeConsole.Data
         {
             get { return _FirstName; }
             set
-            {if(Utilities.IsEmpty(value))
+            {
+                if(Utilities.IsEmpty(value))
                     throw new ArgumentNullException("First name is required.");
                 _FirstName = value;
             }
@@ -38,17 +53,50 @@ namespace PracticeConsole.Data
 
         }
 
+        //composition actually uses the other class as a property/field within
+        //the definition of the class being defined
+
         public ResidentAddress Address;
+
+        //composition
 
         public List<Employment> EmploymentPositions { get; set; }
 
-        public Person()
-        {
-            //if a instance of List<T> is not created and assigned then
-            //  the property will have an initial value of null
-            EmploymentPositions = new List<Employment>();
-        }
+        //public Person()
+        //{
+        //    //if a instance of List<T> is not created and assigned then
+        //    //  the property will have an initial value of null
+        //    EmploymentPositions = new List<Employment>();
 
+        //    //option 1: assign some default value to the strings
+        //    //since FirstName and LastName need to have values
+        //    //you can assign default literials to the properties
+        //    FirstName = "unknown";
+        //    LastName = "unknown";
+        //}
+
+        //option 2
+        //DO NOT code a "Default" constructor
+        //Code ONLY the "Greedy" constructor
+        //if only a greedy constructor exists for the class, the ONLY
+        //  way to possibly create an instance for the class within
+        //  the program is to use the constructor when the class instance
+        //  is created
+
+        public Person(string firstname, string lastname,
+                        List<Employment> employmentpositions,
+                        ResidentAddress address)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            if (employmentpositions != null)
+                EmploymentPositions = employmentpositions;
+            else
+                //allows a null value and the class to have an
+                //  empty List<T>
+                EmploymentPositions = new List<Employment>();
+            Address = address;
+        }
        
     }
 }
