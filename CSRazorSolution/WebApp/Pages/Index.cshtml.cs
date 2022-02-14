ccd35dc52241ccbcf3e8097174ae919dd60dd07f
Don Welch <dwelch@nait.ca>
2022-02-14 16:51:57 -0700

    create razor page web application project, sample OnGet action on Index page

---
+++ CSRazorSolution/WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string MyName { get; set; }
+        public void OnGet()
+        {
+            Random random = new Random();
+            int value = random.Next(0, 100);
+            if (value % 2 == 0)
+            {
+                MyName = $"Don welcome you to the Razor World ({value})";
+            }
+            else
+            {
+                MyName = null;
+            }
+        }
+    }
+}
