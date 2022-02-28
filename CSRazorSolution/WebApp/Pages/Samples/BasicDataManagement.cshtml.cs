b0e54e79d330de4cb1945343ee0dbc17cde27664
Don Welch <dwelch@nait.ca>
2022-02-28 16:51:20 -0700

    implmentation of asp-page-handler buttons

--- CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
+++ CSRazorSolution/WebApp/Pages/Samples/BasicDataManagement.cshtml.cs
@@ -16,8 +16,12 @@ namespace WebApp.Pages.Samples
         [BindProperty]
         public string Comments { get; set; }
 
-        [TempData]
+        //Annotation TempData is required IF you are processing multiple requests (OnPost 
+        //      followed by OnGet) to retain data within the property
+        //[TempData]
         public string FeedBack { get; set; }
+
+
 
         public void OnGet()
         {
@@ -36,6 +40,23 @@ namespace WebApp.Pages.Samples
             //logic that your wish to accomplish should be isolated to the actions
             //  desired for the button
             FeedBack = $"Number {Num}, Course {FavouriteCourse} Comments {Comments}";
+        }
+
+        public void OnPostA()
+        {
+            //process the OnPost request of the form (method="post")
+            //this method is called due to the helper-tag on the form button
+            //the "string" used on the helper-tag asp-page-handler="string" is
+            //   add to the OnPost method name
+            FeedBack = $"Button A was pressed";
+        }
+        public void OnPostB()
+        {
+            //process the OnPost request of the form (method="post")
+            //this method is called due to the helper-tag on the form button
+            //the "string" used on the helper-tag asp-page-handler="string" is
+            //   add to the OnPost method name
+            FeedBack = $"Button B was pressed";
         }
     }
 }
