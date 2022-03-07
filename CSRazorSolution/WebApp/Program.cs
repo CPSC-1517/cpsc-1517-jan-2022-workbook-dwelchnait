0b8036c4e095fa370a8be0c33d1ebbce7f694378
Don Welch <dwelch@nait.ca>
2022-03-07 16:55:01 -0700

    Setup appsettings, setup program.cs, setup Extension class in WestWindSystem

--- CSRazorSolution/WebApp/Program.cs
+++ CSRazorSolution/WebApp/Program.cs
@@ -1,6 +1,24 @@
+#region Additional Namespaces
+using Microsoft.EntityFrameworkCore;
+using WestWindSystem;
+#endregion
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//setup the connection string service for the application
+//1) retreive the connection string information from your appsetting.json
+var connectionString = builder.Configuration.GetConnectionString("WWDB");
+
+//2) register any "services" you wish to use
+//   in our solution our services will be created (coded) in the class library WestWindSystem
+//   one of these services will be the setup of the database context connection
+//   another services will be created as the application requires.
+
+//this setup can be done here, locally
+//this setup can also be done elsewhere and called from this location ****
+builder.Services.WWBackendDependencies(options => options.UseSqlServer(connectionString));
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
---
+++ CSRazorSolution/WestWindSystem/BackendExtensions.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+#region Additional Namespaces
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WestWindSystem.DAL;
+#endregion
+
+namespace WestWindSystem
+{
+    public static class BackendExtensions
+    {
+        public static void WWBackendDependencies(this IServiceCollection services,
+            Action<DbContextOptionsBuilder> options)
+        {
+            //within this method, we will register all the services
+            //that will be used by the system (context setup)
+            //   and will be provided by the system
+
+            //setup the context service
+            services.AddDbContext<WestWindContext>(options);
+        }
+
+    }
+}
