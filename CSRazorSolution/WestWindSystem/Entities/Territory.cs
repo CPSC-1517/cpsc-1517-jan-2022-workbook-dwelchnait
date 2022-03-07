718adaed3def68bb218d51169a074f8239a7493b
Don Welch <dwelch@nait.ca>
2022-03-07 15:48:53 -0700

    create Territory entitiy and WestWind context class

---
+++ CSRazorSolution/WestWindSystem/Entities/Territory.cs
@@ -0,0 +1,29 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace WestWindSystem.Entities
+{ 
+    [Table("Territories")]
+    public class Territory
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [StringLength(20,MinimumLength =1,ErrorMessage ="Territory ID is required and limited to 20 characters")]
+        public string TerritoryID { get; set; }
+
+        [Required(ErrorMessage ="Territory description is required")]
+        [StringLength(50, ErrorMessage = "Territory description is limited to 50 characters")]
+        public string TerritoryDesccription { get; set;}
+        
+        public int RegionID { get; set; }
+    }
+
+}
