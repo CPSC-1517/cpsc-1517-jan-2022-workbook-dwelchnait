8556ec712206a0134d383017521dcb72c349ce13
Don Welch <dwelch@nait.ca>
2022-03-21 16:49:56 -0600

    Using a select ag for replacement of fkey on display, Introduction to Paginator.

--- CSRazorSolution/WebApp/Pages/Samples/PartialFilterSearch.cshtml.cs
+++ CSRazorSolution/WebApp/Pages/Samples/PartialFilterSearch.cshtml.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+
 #region Additional Namespaces
 using WestWindSystem.BLL;       //this is where the services were coded
 using WestWindSystem.Entities;  //this is where the entity definition is coded
+using WebApp.Helpers;           //this is where the Paginator is coded
 #endregion
 
 
@@ -14,12 +16,15 @@ namespace WebApp.Pages.Samples
         #region Private service fields & class constructor
         private readonly ILogger<IndexModel> _logger;
         private readonly TerritoryServices _territoryServices;
+        private readonly RegionServices _regionServices;
 
         public PartialFilterSearchModel(ILogger<IndexModel> logger, 
-            TerritoryServices territoryservices)
+            TerritoryServices territoryservices,
+            RegionServices regionservices)
         {
             _logger = logger;
             _territoryServices = territoryservices;
+            _regionServices = regionservices;
         }
         #endregion
 
@@ -29,13 +34,50 @@ namespace WebApp.Pages.Samples
         [BindProperty(SupportsGet =true)]
         public string searcharg { get; set; }
 
-        public List<Territory> TerritoryInfo { get; set; } = new ();
+        public List<Territory> TerritoryInfo { get; set; }
 
-        public void OnGet()
+        [BindProperty]
+        public List<Region> RegionList { get; set; } = new ();
+
+        #region Paginator
+        //my desired page size
+        private const int PAGE_SIZE = 5;
+        //be able to hold an instance of the Paginator
+        public Paginator Pager { get; set; }
+        #endregion
+
+        public void OnGet(int? currentPage)
         {
+            //using the Paginator with your query
+
+            //OnGet will have a parameter (Request query string) that receives the
+            //  current page number. On the initial load of the page, this value
+            //  will be null.
+
+            //obtain the data list for the Region dropdownlist (select tag)
+            RegionList = _regionServices.Region_List();
             if (!string.IsNullOrWhiteSpace(searcharg))
             {
-                TerritoryInfo = _territoryServices.GetByPartialDescription(searcharg);
+                //setting up for using the Paginator only needs to be done if
+                //  a query is executing
+
+                //determine the current page number
+                int pagenumber = currentPage.HasValue ? currentPage.Value : 1;
+                //setup the current state of the paginator (sizing)
+                PageState current = new(pagenumber, PAGE_SIZE);
+                //temporary local integer to hold the results of the query's total collection size
+                //  this will be need by the Paginator during the paginator's execution
+                int totalcount;
+
+                //we need to pass paging data into our query so that efficiencies in the
+                //  system will ONLY return the amount of records to actually be display
+                //  on the browser page.
+
+                TerritoryInfo = _territoryServices.GetByPartialDescription(searcharg,
+                                    pagenumber, PAGE_SIZE, out totalcount);
+
+                //create the needed Pagnator instance
+                Pager = new Paginator(totalcount, current);
             }
         }
 
--- CSRazorSolution/WestWindSystem/BLL/TerritoryServices.cs
+++ CSRazorSolution/WestWindSystem/BLL/TerritoryServices.cs
@@ -29,12 +29,37 @@ namespace WestWindSystem.BLL
         #region Queries
         
         //query by a string
-        public List<Territory> GetByPartialDescription(string partialdescription)
+        //this partial search query has been alter to allow for paging of its results
+        //IF paging is NOT required the the query should have a single string parameter: partialdescription
+
+        public List<Territory> GetByPartialDescription(string partialdescription,
+                                                        int pagenumber,
+                                                        int pagesize,
+                                                        out int totalcount)
         {
             IEnumerable<Territory> info = _context.Territories
                             .Where(x => x.TerritoryDescription.Contains(partialdescription))
                             .OrderBy(x => x.TerritoryDescription);
-            return info.ToList();
+
+            //using the paging parameters to obtain only the necessary rows that
+            //  will be shown by the Paginator
+
+            //determine the total collection size of our query
+            totalcount = info.Count();
+            //determine the number of rows to skip
+            //this skipped count reflects the rows of the previous pages
+            //remember the pagenumber is a natural number (1,2,3,...)
+            //this needs to be treated as an index (natural number - 1)
+            //the number of rows to skip is index * pagesize
+            int skipRows = (pagenumber - 1) * pagesize;
+            //return only the required number of rows.
+            //this will be done using filters belonging to Linq
+            //use the filter .Skip(n) to skip over n rows from the beginning of a collection
+            //use the filter .Take(n) to take the next n rows from a collection
+            return info.Skip(skipRows).Take(pagesize).ToList();
+
+            //this is the return statement that would be used IF no paging is being implemented
+            //return info.ToList();
         }
         //query by a number
         public List<Territory> GetByRegion(int regionid)
